cf50ad8559a1906ee140e52cd4eb1d9e29f64f49
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-04-08 09:52:23 -0300

    Transição de javascript para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,17 @@ import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss';
 
+
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string
+}
+
 // https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
